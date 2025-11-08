c5582a4274d916bf6566dff9764095511c88ab5b
@UhLukas <119970521+UhLukas@users.noreply.github.com>
2025-11-08 21:20:47 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,29 @@
+const CACHE_NAME = "jira-cache-v2";
+const JIRA_PROJECT_URL = "https://discord-account-support-corp.atlassian.net/jira/polaris/projects/COMMUNITY/ideas/view/8270241";
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("jira-cache").then(cache => {
-      return cache.addAll(["/"]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(["/", JIRA_PROJECT_URL]);
     })
   );
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+  const url = e.request.url;
+
+  if (url.startsWith("https://discord-account-support-corp.atlassian.net/jira/polaris/projects/COMMUNITY/")) {
+    e.respondWith(
+      caches.match(e.request).then(response => {
+        return response || fetch(e.request).then(fetchRes => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(e.request, fetchRes.clone());
+            return fetchRes;
+          });
+        });
+      })
+    );
+  } else {
+    e.respondWith(fetch(e.request));
+  }
 });
